73bcb432c9992e0d8acd966eda4047326f29df62
clinton-lynx <clintonadeoti1@gmail.com>
2024-09-07 14:51:02 +0100

    feat: add contact logic

--- routes/contact.js
+++ routes/contact.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const contactsController = require('../controllers/contact');
 
 router.post('/contact', contactsController.submitForm);
+router.get('/', (req,res) =>{
+    res.send("success")
+});
 
 module.exports = router;
